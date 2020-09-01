9d2e6d4fda3d2ed037c91a71352a8d2cf267628c
pamitkumar <pandaamit@gmail.com>
2020-09-01 16:47:24 +0530

    Create BreadcrumbsViewModel.cs

---
+++ NISSitecore/src/Feature/Navigation/code/ViewModels/BreadcrumbsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NISSitecore.Feature.Navigation.ViewModels
+{
+    public class BreadcrumbsViewModel
+    {
+        public List<Models.IList> Trail { get; set; }
+        public Guid ActiveItemId { get; set; }
+        public bool HideBreadcrumb { get; set; }
+    }
+}
