5334d3065ddd62b1c1d87f7fce1634c9018b69b7
pamitkumar <pandaamit@gmail.com>
2020-09-01 16:45:53 +0530

    breadcrumbs
    breadcrumb

--- NISSitecore/src/Feature/Navigation/code/Controllers/NavigationController.cs
+++ NISSitecore/src/Feature/Navigation/code/Controllers/NavigationController.cs
@@ -1,4 +1,6 @@
-﻿using NISSitecore.Feature.Navigation.Models;
+﻿using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Web;
+using NISSitecore.Feature.Navigation.Models;
 using NISSitecore.Foundation.Content.Repositories;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
@@ -67,6 +69,56 @@ namespace NISSitecore.Feature.Navigation.Controllers
 
             }
             return View(datasource);
+        }
+
+        public ActionResult BreadCrumbs()
+        {
+            //var context = new IRequestContext();
+            Item rootItem = Sitecore.Context.Database.Items[Sitecore.Context.Site.StartPath].Parent;
+            Item currentItem = Sitecore.Context.Item;
+
+            //var datasource = _renderingRepository.GetRenderingItem<IListItems>();
+            //var root = _contextRepository.GetHomeItem<IList>();
+
+            ViewModels.BreadcrumbsViewModel model = new ViewModels.BreadcrumbsViewModel();
+            model.Trail = new List<Models.IList>();
+            model.ActiveItemId = currentItem.ID.Guid;
+
+            while (currentItem != null && currentItem.ID != rootItem.ID)
+            {
+                var options = new GetItemByIdOptions(currentItem.ID.Guid)
+                {
+                    InferType = true,
+                    Lazy = Glass.Mapper.LazyLoading.Disabled
+                };
+                Models.IList currentMenuItem = _contentRepository.GetItem<Models.IList>(options);
+                if (currentMenuItem == null)
+                {
+                    break;
+                }
+                
+                model.Trail.Add(currentMenuItem);
+
+
+                //if current item says hide from breadcrumbs then turn off breadcrumbs for entire path.
+                if (currentMenuItem.Id == model.ActiveItemId && currentMenuItem.HideFromBreadcrumbs)
+                {
+                    model.Trail = new List<Models.IList>();
+                    model.HideBreadcrumb = true;
+                    break;
+                }
+
+                //skip over items above it in chain;
+                if (currentMenuItem.HideFromBreadcrumbs)
+                    model.Trail.RemoveAt(model.Trail.Count - 1);
+                currentItem = currentItem.Parent;
+            }
+            if (model.HideBreadcrumb == false && model.Trail.Count > 1)
+                model.Trail.Reverse();
+
+            
+            return View(model);
+
         }
     }
 }
